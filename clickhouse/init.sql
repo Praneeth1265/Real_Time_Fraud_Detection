cce4009bbef5c0b64192eb3380bd1b77f7d32c8a
Praneeth Gourabattuni <gpraneeth555@gmail.com>
2026-01-24 13:11:31 +0530

    Updated booting-clickhouse schema

--- clickhouse/init.sql
+++ clickhouse/init.sql
@@ -1,11 +1,24 @@
 CREATE DATABASE IF NOT EXISTS fraud;
+USE fraud;
 
-CREATE TABLE IF NOT EXISTS fraud.transactions
+CREATE TABLE IF NOT EXISTS transactions
 (
     user_id UInt32,
     amount Float64,
     location String,
-    timestamp DateTime
+
+    lat Float64,
+    lon Float64,
+
+    timestamp DateTime,
+
+    avg_amount Float64,
+    distance_from_last_tx Float64,
+    hours_since_last_tx Float64,
+    location_mismatch UInt8,
+    is_first_tx UInt8,
+    fraud_probability Float64
 )
 ENGINE = MergeTree
 ORDER BY (user_id, timestamp);
+
